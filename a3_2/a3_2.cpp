eeb5256d066385761d2290ad8037f00bf44dad25
Josie Lipkin <jlipkin8@gmail.com>
2016-01-23 20:51:43 -0800

    Initial Commit

---
+++ a3_2/a3_2.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  a3_2
+//
+//  Created by Josie Lipkin on 1/23/16.
+//  Copyright © 2016 Josie Lipkin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
