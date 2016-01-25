a029c1103ea5469ca8b1576b32e1d0af6dc2925e
Josie Lipkin <jlipkin8@gmail.com>
2016-01-24 16:28:54 -0800

    This is the code provided by the teacher

--- a3_2/a3_2.cpp
+++ a3_2/a3_2.cpp
@@ -1,5 +1,5 @@
 //
-//  main.cpp
+//  a3_2.cpp
 //  a3_2
 //
 //  Created by Josie Lipkin on 1/23/16.
@@ -7,9 +7,103 @@
 //
 
 #include <iostream>
+#include <time.h> 
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+const int MAT_SIZE = 100;
+typedef float* DynMat[MAT_SIZE];
+void matShowDyn( const DynMat & matA, int start, int size);
+void matMultDyn( const DynMat & matA, const DynMat & matB, DynMat & matC, int size);
+int main() {
+    
+    int r, c;
+    clock_t startTime, stopTime;
+    float randFrac;
+    int randRow, randCol, smallPercent;
+    
+    //non-sparse dynamic matrix
+    DynMat matDyn, matDynAns;
+    
+    //allocate rows and initialize to 0
+    for(r = 0; r < MAT_SIZE; r++)
+    {
+        //?
+        for(c = 0; c < MAT_SIZE; c++)
+        {
+            //?
+        }
+    }
+    
+    //generate small non-default values between .1 and 10
+    smallPercent = MAT_SIZE/20. * MAT_SIZE; //div by 20. means 5%
+    for( r = 0; r < smallPercent; r++)
+    {
+        //??
+    }
+    
+    //10 x 10 submatrix in lower right
+    matShowDyn(matDyn, MAT_SIZE - 10, 10);
+    
+    startTime = clock(); //start
+    matMultDyn(matDyn, matDyn, matDynAns, MAT_SIZE);
+    stopTime = clock(); //stop
+    
+    matShowDyn(matDynAns, MAT_SIZE - 10, 10);
+    cout << "\nSize = " << MAT_SIZE << " Dyn Array Mult Elapsed Time: "
+        << (double)(stopTime - startTime)/(double)CLOCKS_PER_SEC
+        << " seconds." << endl << endl;
+    
+    //clean up
+    for ( r = 0; r < MAT_SIZE; r++)
+        // ??
+        
+    cout << endl;
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
